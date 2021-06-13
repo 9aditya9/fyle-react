0c804c696d41017b950e09b7333f08ff0641fb58
9aditya9 <learning.digital558@gmail.com>
2021-06-13 21:05:46 +0530

    updated  links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ function Header() {
 		<>
 			<nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ backgroundColor: 'aliceblue' }}>
 				<div className="container-fluid" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-					<Link to="/" style={{ color: 'rgb(157, 5, 212)' }} className="navbar-brand nav-title mb-2 mb-lg-0" >Home</Link>
+					<Link to="/fyle-react" style={{ color: 'rgb(157, 5, 212)' }} className="navbar-brand nav-title mb-2 mb-lg-0" >Home</Link>
 					<div style={{color: 'rgb(157, 5, 212)', marginLeft: '20px', marginRight: '20px', fontSize: '25px'}}>FYLE TASK</div>
-					<Link to="/favorites" style={{ color: 'rgb(157, 5, 212)' }} className="navbar-brand nav-title mb-2 mb-lg-0" >Favorites</Link>
+					<Link to="/fyle-react/favorites" style={{ color: 'rgb(157, 5, 212)' }} className="navbar-brand nav-title mb-2 mb-lg-0" >Favorites</Link>
 				</div>
 			</nav>
 		</>
